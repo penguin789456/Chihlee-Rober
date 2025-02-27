f543c444610d6ce8ad7d80037aa533c751dfb3ae
AVES <binhome219@gmail.com>
2025-02-28 02:46:28 +0800

    刪除 jsontest.py 檔案

--- jsontest.py
+++
@@ -1,6 +0,0 @@
-import json   
-
-with open(r"C:\Users\binho\Downloads\Chihlee Rober\CertAnser\campaign-manager-certification-answers.json", "r", encoding="utf-8") as json_file:
-    data = json.load(json_file)
-    for i in data:
-        print(i[0])
